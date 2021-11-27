e131d5e1ed69c8c9fbcca31a99791cf63abbf5e6
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2021-11-27 13:29:35 +0530

    Initial commit

---
+++ Smart Ventilator/main.c
@@ -0,0 +1,18 @@
+/*
+ * Smart Ventilator.c
+ *
+ * Created: 11/27/2021 1:26:16 PM
+ * Author : Yesitha Sathsara
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
