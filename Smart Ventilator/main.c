8552db5141470b83c3bcbf418749353611f95ccf
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2021-11-30 22:37:04 +0530

    update main.c

--- Smart Ventilator/main.c
+++ Smart Ventilator/main.c
@@ -15,7 +15,7 @@ int oxygenTankPercentage();
 
 void notifyGSM(const char *string, int percentage);
 
-void notifySecondaryDisplay();
+void notifyDisplay();
 
 bool automationOn();
 
@@ -27,13 +27,30 @@ int PatientTemp();
 
 int checkBloodOxygenLevel();
 
-void startOxygenAndAirSupply();
+
 
 void startAirSupply();
 
+void notifySpeaker();
+
+bool turnOn();
+
+void startStepperMotor(int breathPerMin, int BreathLength);
+
+void startOxygenAndAirSupply(int percentage);
+
+void controlSolenoidValve(int oxygenPercentage, int breathPerMin);
+
+int calculateConstant(int pressure, int density);
+
+int getOxygenTankPressure();
+
+int calculateAnotherTime(int value);
+
 int Average_Blood_Oxygen_level=97;
-int Average_Breath_length=500;
+int Average_Breath_length=50;
 int Average_Breath_Per_Min=18;
+int Oxygen_percentage=60;
 
 int main(void)
 {
@@ -47,65 +64,128 @@ int main(void)
 				if(checkPatientTemp()){
 
 					if(checkBloodOxygenLevel() < Average_Blood_Oxygen_level){
-						startOxygenAndAirSupply();
+						startOxygenAndAirSupply(Oxygen_percentage);
 						}else{
 						startAirSupply();
 					}
 				}
 				}else{
+				checkPatientTemp();
 				getParametersFromKnobs();
+				startOxygenAndAirSupply(Oxygen_percentage);
 			}
 			}else{return 0;}
 		}
 	}
 
+	void startOxygenAndAirSupply(int percentage) {
+		startStepperMotor(Average_Breath_Per_Min, Average_Breath_length);
+		controlSolenoidValve(Oxygen_percentage, Average_Breath_Per_Min);
+	}
+
+	void controlSolenoidValve(int oxygenPercentage, int breathPerMin) {
+		int inflationTime=30/breathPerMin;
+		int tAir;//Air Solenoid Valve Time
+		int tOxygen;//Oxygen Solenoid Valve Time
+		int constValue;
+		int Pressure;
+		int airDensity;
+		int OxygenDensity;
+		if(tAir>tOxygen){
+			tAir=inflationTime;
+			constValue=calculateConstant(Pressure, airDensity);
+			tOxygen=calculateAnotherTime(constValue);
+			} else{
+			tOxygen=inflationTime;
+			
+			constValue=calculateConstant(getOxygenTankPressure(), OxygenDensity);
+			tAir= calculateAnotherTime(constValue);
+		}
+	}
+
+	int calculateAnotherTime(int value) {
+		return 0;
+	}
+
+	int getOxygenTankPressure() {
+		return 0;//return Oxygen tank pressure
+	}
+
+	int calculateConstant(int pressure, int density) {
+		return 1;
+	}
+
 	void startAirSupply() {
+		startStepperMotor(Average_Breath_Per_Min, Average_Breath_length);
+		controlSolenoidValve(100, Average_Breath_Per_Min);
+	}
+
+	void startStepperMotor(int breathPerMin, int BreathLength) {
+
 
 	}
 
-	void startOxygenAndAirSupply() {
 
-	}
 
 	int checkBloodOxygenLevel() {
-		return 0;
+
+		return 0;//return Blood Oxygen Level
 	}
 
 	int PatientTemp() {
 		return 0;//return temperature value
 	}
 
+	void notifySpeaker() {
+
+	}
+
 	bool checkPatientTemp() {
+		if(PatientTemp()>37.2||PatientTemp()<36.1){
+			notifyGSM("Temperature Not Normal",PatientTemp());
+			notifyDisplay();
+			notifySpeaker();
+			return 0;
+			}else{
+			return 1;
+		}
 		//if normal return 1 else notify speaker and gsm
-		return 1;
 	}
 
 	void getParametersFromKnobs() {
-		//get values and set to variables
+
+		//get values Breath per min,Oxygen Percentage,Breath Length
 	}
 
 	bool automationOn() {
-		return 1;
+		return 1;//Check Automation On/Off
+	}
+
+	bool turnOn() {
+		return 1;//return 1 if power on switched pressed else return 0
 	}
 
 	bool checkStatus() {
-		if(oxygenTankPercentage() < 10){
-			notifyGSM("Oxygen Tank Percentage : ",oxygenTankPercentage());
-			notifySecondaryDisplay();
-			return 0;
-			}else{return 1;}
-		}
+		if (turnOn()) {
+			if (oxygenTankPercentage() < 10) {
+				notifyGSM("Oxygen Tank Percentage : ", oxygenTankPercentage());
+				notifyDisplay();
+				return 0;
+				} else { return 1; }
+				}else{return 0;}
+			}
 
-		void notifySecondaryDisplay() {
+			void notifyDisplay() {
 
-		}
+			}
 
-		void notifyGSM(const char *string, int percentage) {
+			void notifyGSM(const char *string, int percentage) {
 
-		}
+			}
 
-		int oxygenTankPercentage() {
-			return 1;
-		}
+			int oxygenTankPercentage() {
+				//return oxygen tank percentage
+				return 1;
+			}
 
 
